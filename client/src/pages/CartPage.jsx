a769eae450c033514093c62f463d75f36ebd9710
Jonibek <94430009+MJony05@users.noreply.github.com>
2023-07-20 22:13:46 +0500

    project stopped temporary

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,8 +1,19 @@
 import { useContext } from 'react'
 import { CartContext } from '../CartContext'
-import { Button } from '@mui/material'
+import { Button, TextField } from '@mui/material'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import AddIcon from '@mui/icons-material/Add'
+import CloseIcon from '@mui/icons-material/Close'
+import RemoveIcon from '@mui/icons-material/Remove'
 import './cart.css'
 const CartPage = () => {
+  const url = process.env.REACT_APP_API_URL
   const { cartItems, removeFromCart, updateQuantity, clearCart } =
     useContext(CartContext)
 
@@ -35,62 +46,106 @@ const CartPage = () => {
   }
 
   return (
-    <div>
-      <h2>Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Subtotal</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartItems.map((item) => (
-              <tr key={item.product._id}>
-                <td>{item.product.name}</td>
-                <td>{item.product.price}</td>
-                <td>
-                  <Button
-                    onClick={() => handleDecreaseQuantity(item.product._id)}
-                  >
-                    -
-                  </Button>
-                  <span>{item.quantity}</span>
-                  <Button
-                    onClick={() => handleIncreaseQuantity(item.product._id)}
-                  >
-                    +
-                  </Button>
-                </td>
-                <td>{calculateSubtotal(item.product.price, item.quantity)}</td>
-                <td>
-                  <Button onClick={() => handleRemoveItem(item.product._id)}>
-                    Remove
-                  </Button>
-                </td>
-              </tr>
-            ))}
-            <tr>
-              <td colSpan={3} align="right">
-                Total:
-              </td>
-              <td align="right">{calculateTotal()}</td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      )}
-      {cartItems.length > 0 && (
-        <Button variant="contained" onClick={handleClearCart}>
-          Clear Cart
-        </Button>
-      )}
+    <div className="cart-page">
+      <div className="container">
+        <h2>Savat</h2>
+        {cartItems.length === 0 ? (
+          <p>Your cart is empty.</p>
+        ) : (
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow sx={{ backgroundColor: '#eee' }}>
+                  <TableCell width={'120px'}>Rasm</TableCell>
+                  <TableCell>Product</TableCell>
+                  <TableCell align="right">Price</TableCell>
+                  <TableCell align="center">Quantity</TableCell>
+                  <TableCell width={'250px'} align="right">
+                    Subtotal
+                  </TableCell>
+                  <TableCell width={'60px'}></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {cartItems.map((item) => (
+                  <TableRow key={item.product._id}>
+                    <TableCell>
+                      <img src={url + item.product.image} width="90px" alt="" />
+                    </TableCell>
+                    <TableCell>{item.product.name}</TableCell>
+                    <TableCell align="right">
+                      {item.product.price} so'm
+                    </TableCell>
+                    <TableCell>
+                      <div className="input-quantity">
+                        <div className="input-quantity-dec">
+                          <RemoveIcon
+                            fontSize="small"
+                            onClick={() => {
+                              if (item.quantity > 1)
+                                handleDecreaseQuantity(item.product._id)
+                            }}
+                          />
+                        </div>
+                        <TextField
+                          className="input-quantity-input"
+                          size="small"
+                          value={item.quantity}
+                          inputProps={{
+                            style: { textAlign: 'center' },
+                            min: 1,
+                            max: 100,
+                          }}
+                          onChange={(e) => {
+                            if (typeof parseInt(e.target.value) === 'number') {
+                              updateQuantity(
+                                item.product._id,
+                                e.target.value - item.quantity
+                              )
+                            }
+                          }}
+                        />
+                        <div className="input-quantity-inc">
+                          <AddIcon
+                            fontSize="small"
+                            onClick={() =>
+                              handleIncreaseQuantity(item.product._id)
+                            }
+                          />
+                        </div>
+                      </div>
+                    </TableCell>
+
+                    <TableCell align="right">
+                      {calculateSubtotal(item.product.price, item.quantity)}{' '}
+                      so'm
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        onClick={() => handleRemoveItem(item.product._id)}
+                      >
+                        <CloseIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow>
+                  <TableCell colSpan={3} align="right">
+                    Total:
+                  </TableCell>
+                  <TableCell align="right">{calculateTotal()}</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+        {cartItems.length > 0 && (
+          <Button variant="contained" onClick={handleClearCart}>
+            Clear Cart
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
